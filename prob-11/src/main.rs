a02728c339b2ddef51e19e9e4907fdebbee83393
Wesley Jon Cheek <w-cheek@satake-japan.co.jp>
2025-03-13 11:06:02 +0900

    Prob11 solved

--- prob-11/src/main.rs
+++ prob-11/src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashSet, error::Error, fs};
 
 #[derive(PartialEq, Debug, Hash, Eq)]
-enum AcceptableDirections {
+enum AcceptableDirection {
     Up,
     UpRight,
     Right,
@@ -17,35 +17,94 @@ struct Coordinates {
     col_num: usize,
 }
 
-fn get_acceptable_directions(coords: Coordinates) -> HashSet<AcceptableDirections> {
-    let mut acceptable_directions: HashSet<AcceptableDirections> = HashSet::new();
+fn get_acceptable_directions(coords: Coordinates) -> HashSet<AcceptableDirection> {
+    let mut acceptable_directions: HashSet<AcceptableDirection> = HashSet::new();
+    const START_EDGE: usize = 2;
+    const END_EDGE: usize = 17;
 
-    if coords.row_num > 3 {
-        acceptable_directions.insert(AcceptableDirections::Up);
-        if coords.col_num < 16 {
-            acceptable_directions.insert(AcceptableDirections::UpRight);
+    if coords.row_num > START_EDGE {
+        acceptable_directions.insert(AcceptableDirection::Up);
+        if coords.col_num < END_EDGE {
+            acceptable_directions.insert(AcceptableDirection::UpRight);
         }
     }
-    if coords.col_num < 16 {
-        acceptable_directions.insert(AcceptableDirections::Right);
-        if coords.row_num < 16 {
-            acceptable_directions.insert(AcceptableDirections::DownRight);
+    if coords.col_num < END_EDGE {
+        acceptable_directions.insert(AcceptableDirection::Right);
+        if coords.row_num < END_EDGE {
+            acceptable_directions.insert(AcceptableDirection::DownRight);
         }
     }
-    if coords.row_num < 16 {
-        acceptable_directions.insert(AcceptableDirections::Down);
-        if coords.col_num > 3 {
-            acceptable_directions.insert(AcceptableDirections::DownLeft);
+    if coords.row_num < END_EDGE {
+        acceptable_directions.insert(AcceptableDirection::Down);
+        if coords.col_num > START_EDGE {
+            acceptable_directions.insert(AcceptableDirection::DownLeft);
         }
     }
-    if coords.col_num > 3 {
-        acceptable_directions.insert(AcceptableDirections::Left);
-        if coords.row_num > 3 {
-            acceptable_directions.insert(AcceptableDirections::UpLeft);
+    if coords.col_num > START_EDGE {
+        acceptable_directions.insert(AcceptableDirection::Left);
+        if coords.row_num > START_EDGE {
+            acceptable_directions.insert(AcceptableDirection::UpLeft);
         }
     }
 
     acceptable_directions
+}
+
+fn get_product(
+    grid: &Vec<Vec<u32>>,
+    coordinates: Coordinates,
+    for_direction: AcceptableDirection,
+) -> u32 {
+    match for_direction {
+        AcceptableDirection::Up => {
+            grid[coordinates.row_num][coordinates.col_num]
+                * grid[coordinates.row_num - 1][coordinates.col_num]
+                * grid[coordinates.row_num - 2][coordinates.col_num]
+                * grid[coordinates.row_num - 3][coordinates.col_num]
+        }
+        AcceptableDirection::UpRight => {
+            grid[coordinates.row_num][coordinates.col_num]
+                * grid[coordinates.row_num - 1][coordinates.col_num + 1]
+                * grid[coordinates.row_num - 2][coordinates.col_num + 2]
+                * grid[coordinates.row_num - 3][coordinates.col_num + 3]
+        }
+        AcceptableDirection::Right => {
+            grid[coordinates.row_num][coordinates.col_num]
+                * grid[coordinates.row_num][coordinates.col_num + 1]
+                * grid[coordinates.row_num][coordinates.col_num + 2]
+                * grid[coordinates.row_num][coordinates.col_num + 3]
+        }
+        AcceptableDirection::DownRight => {
+            grid[coordinates.row_num][coordinates.col_num]
+                * grid[coordinates.row_num + 1][coordinates.col_num + 1]
+                * grid[coordinates.row_num + 2][coordinates.col_num + 2]
+                * grid[coordinates.row_num + 3][coordinates.col_num + 3]
+        }
+        AcceptableDirection::Down => {
+            grid[coordinates.row_num][coordinates.col_num]
+                * grid[coordinates.row_num + 1][coordinates.col_num]
+                * grid[coordinates.row_num + 2][coordinates.col_num]
+                * grid[coordinates.row_num + 3][coordinates.col_num]
+        }
+        AcceptableDirection::DownLeft => {
+            grid[coordinates.row_num][coordinates.col_num]
+                * grid[coordinates.row_num + 1][coordinates.col_num - 1]
+                * grid[coordinates.row_num + 2][coordinates.col_num - 2]
+                * grid[coordinates.row_num + 3][coordinates.col_num - 3]
+        }
+        AcceptableDirection::Left => {
+            grid[coordinates.row_num][coordinates.col_num]
+                * grid[coordinates.row_num][coordinates.col_num - 1]
+                * grid[coordinates.row_num][coordinates.col_num - 2]
+                * grid[coordinates.row_num][coordinates.col_num - 3]
+        }
+        AcceptableDirection::UpLeft => {
+            grid[coordinates.row_num][coordinates.col_num]
+                * grid[coordinates.row_num - 1][coordinates.col_num - 1]
+                * grid[coordinates.row_num - 2][coordinates.col_num - 2]
+                * grid[coordinates.row_num - 3][coordinates.col_num - 3]
+        }
+    }
 }
 
 fn get_num_grid() -> Result<Vec<Vec<u32>>, Box<dyn Error>> {
@@ -63,14 +122,21 @@ fn get_num_grid() -> Result<Vec<Vec<u32>>, Box<dyn Error>> {
 }
 
 fn main() {
+    let mut products: Vec<u32> = vec![];
     if let Ok(grid) = get_num_grid() {
         for (row_num, row) in grid.iter().enumerate() {
-            for (col_num, col) in row.iter().enumerate() {
-                let acceptable_directions =
-                    get_acceptable_directions(Coordinates { row_num, col_num });
+            for (col_num, _col) in row.iter().enumerate() {
+                for direction in get_acceptable_directions(Coordinates { row_num, col_num }) {
+                    products.push(get_product(
+                        &grid,
+                        Coordinates { row_num, col_num },
+                        direction,
+                    ))
+                }
             }
         }
     }
+    println!("{}", products.iter().max().unwrap())
 }
 
 #[cfg(test)]
@@ -100,9 +166,9 @@ mod test {
         assert_eq!(
             acceptable_directions,
             HashSet::from([
-                AcceptableDirections::Right,
-                AcceptableDirections::Down,
-                AcceptableDirections::DownRight,
+                AcceptableDirection::Right,
+                AcceptableDirection::Down,
+                AcceptableDirection::DownRight,
             ])
         );
 
@@ -115,9 +181,9 @@ mod test {
         assert_eq!(
             acceptable_directions,
             HashSet::from([
-                AcceptableDirections::Up,
-                AcceptableDirections::UpLeft,
-                AcceptableDirections::Left,
+                AcceptableDirection::Up,
+                AcceptableDirection::UpLeft,
+                AcceptableDirection::Left,
             ])
         );
 
@@ -130,15 +196,50 @@ mod test {
         assert_eq!(
             acceptable_directions,
             HashSet::from([
-                AcceptableDirections::Right,
-                AcceptableDirections::Down,
-                AcceptableDirections::DownRight,
-                AcceptableDirections::Up,
-                AcceptableDirections::UpLeft,
-                AcceptableDirections::Left,
-                AcceptableDirections::DownLeft,
-                AcceptableDirections::UpRight
+                AcceptableDirection::Right,
+                AcceptableDirection::Down,
+                AcceptableDirection::DownRight,
+                AcceptableDirection::Up,
+                AcceptableDirection::UpLeft,
+                AcceptableDirection::Left,
+                AcceptableDirection::DownLeft,
+                AcceptableDirection::UpRight
             ])
         );
+    }
+
+    #[test]
+    fn test_get_product() {
+        let grid = get_num_grid().unwrap();
+
+        let product = get_product(
+            &grid,
+            Coordinates {
+                row_num: 0,
+                col_num: 0,
+            },
+            AcceptableDirection::Right,
+        );
+        assert_eq!(product, 34144);
+
+        let product = get_product(
+            &grid,
+            Coordinates {
+                row_num: 0,
+                col_num: 0,
+            },
+            AcceptableDirection::DownRight,
+        );
+        assert_eq!(product, 279496);
+
+        let product = get_product(
+            &grid,
+            Coordinates {
+                row_num: 0,
+                col_num: 0,
+            },
+            AcceptableDirection::Down,
+        );
+        assert_eq!(product, 1_651_104);
     }
 }
