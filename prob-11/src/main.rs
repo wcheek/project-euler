c9659ece64eb53a2a81c6fe0095d5748d2b935cb
Wesley Jon Cheek <w-cheek@satake-japan.co.jp>
2025-03-13 10:22:35 +0900

    Getting acceptable directions

--- prob-11/src/main.rs
+++ prob-11/src/main.rs
@@ -1,4 +1,52 @@
-use std::{error::Error, fs};
+use std::{collections::HashSet, error::Error, fs};
+
+#[derive(PartialEq, Debug, Hash, Eq)]
+enum AcceptableDirections {
+    Up,
+    UpRight,
+    Right,
+    DownRight,
+    Down,
+    DownLeft,
+    Left,
+    UpLeft,
+}
+
+struct Coordinates {
+    row_num: usize,
+    col_num: usize,
+}
+
+fn get_acceptable_directions(coords: Coordinates) -> HashSet<AcceptableDirections> {
+    let mut acceptable_directions: HashSet<AcceptableDirections> = HashSet::new();
+
+    if coords.row_num > 3 {
+        acceptable_directions.insert(AcceptableDirections::Up);
+        if coords.col_num < 16 {
+            acceptable_directions.insert(AcceptableDirections::UpRight);
+        }
+    }
+    if coords.col_num < 16 {
+        acceptable_directions.insert(AcceptableDirections::Right);
+        if coords.row_num < 16 {
+            acceptable_directions.insert(AcceptableDirections::DownRight);
+        }
+    }
+    if coords.row_num < 16 {
+        acceptable_directions.insert(AcceptableDirections::Down);
+        if coords.col_num > 3 {
+            acceptable_directions.insert(AcceptableDirections::DownLeft);
+        }
+    }
+    if coords.col_num > 3 {
+        acceptable_directions.insert(AcceptableDirections::Left);
+        if coords.row_num > 3 {
+            acceptable_directions.insert(AcceptableDirections::UpLeft);
+        }
+    }
+
+    acceptable_directions
+}
 
 fn get_num_grid() -> Result<Vec<Vec<u32>>, Box<dyn Error>> {
     let mut grid: Vec<Vec<u32>> = vec![];
@@ -15,7 +63,14 @@ fn get_num_grid() -> Result<Vec<Vec<u32>>, Box<dyn Error>> {
 }
 
 fn main() {
-    let grid = get_num_grid();
+    if let Ok(grid) = get_num_grid() {
+        for (row_num, row) in grid.iter().enumerate() {
+            for (col_num, col) in row.iter().enumerate() {
+                let acceptable_directions =
+                    get_acceptable_directions(Coordinates { row_num, col_num });
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -32,5 +87,58 @@ mod test {
         assert_eq!(grid[0][0], 8);
         assert_eq!(grid[19][0], 1);
         assert_eq!(grid[9][19], 95);
+    }
+
+    #[test]
+    fn test_get_acceptable_directions() {
+        let acceptable_directions = get_acceptable_directions(Coordinates {
+            row_num: 0,
+            col_num: 0,
+        });
+
+        assert!(!acceptable_directions.is_empty());
+        assert_eq!(
+            acceptable_directions,
+            HashSet::from([
+                AcceptableDirections::Right,
+                AcceptableDirections::Down,
+                AcceptableDirections::DownRight,
+            ])
+        );
+
+        let acceptable_directions = get_acceptable_directions(Coordinates {
+            row_num: 19,
+            col_num: 19,
+        });
+
+        assert!(!acceptable_directions.is_empty());
+        assert_eq!(
+            acceptable_directions,
+            HashSet::from([
+                AcceptableDirections::Up,
+                AcceptableDirections::UpLeft,
+                AcceptableDirections::Left,
+            ])
+        );
+
+        let acceptable_directions = get_acceptable_directions(Coordinates {
+            row_num: 10,
+            col_num: 10,
+        });
+
+        assert!(!acceptable_directions.is_empty());
+        assert_eq!(
+            acceptable_directions,
+            HashSet::from([
+                AcceptableDirections::Right,
+                AcceptableDirections::Down,
+                AcceptableDirections::DownRight,
+                AcceptableDirections::Up,
+                AcceptableDirections::UpLeft,
+                AcceptableDirections::Left,
+                AcceptableDirections::DownLeft,
+                AcceptableDirections::UpRight
+            ])
+        );
     }
 }
